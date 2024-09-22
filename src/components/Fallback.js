9db6079dc560fec80a8294eb91c1f265a3242dce
ACORBERAND <antoinecobertondefleury@gmail.com>
2024-09-22 18:13:19 +0200

    add description

--- src/components/Fallback.js
+++ src/components/Fallback.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
-import './Fallback.scss'; // Assure-toi de bien inclure ce fichier SCSS
+import './Fallback.scss'; 
 
 export default function Fallback() {
-  // Déclare une variable d'état pour stocker le pourcentage
   const [progress, setProgress] = useState(1);
 
-  // Utilise useEffect pour mettre à jour le pourcentage de chargement
+  //met à jour le pourcentage de chargement
   useEffect(() => {
     const interval = setInterval(() => {
       // Incrémente le pourcentage jusqu'à 100%
       setProgress((prevProgress) =>
         prevProgress < 100 ? prevProgress + 1 : 100
       );
-    }, 100); // Change toutes les 100ms (0.1s)
+    }, 100); 
 
     // Nettoie l'intervalle lorsque le composant est démonté
     return () => clearInterval(interval);
@@ -22,7 +21,8 @@ export default function Fallback() {
     <div className="fallback">
       <div className="loader">
         <h1>Chargement</h1>
-        <span>{progress} %</span> {/* Affiche la progression */}
+        {/* Affiche la progression */}
+        <span>{progress} %</span> 
         
         {/* Barre de progression */}
         <div className="progress-bar">
