7d2df937a70be844134fe921ac87271c674ee09a
ACORBERAND <antoinecobertondefleury@gmail.com>
2024-09-22 18:10:24 +0200

    Loader page

--- src/components/Fallback.js
+++ src/components/Fallback.js
@@ -1,12 +1,36 @@
+import React, { useState, useEffect } from "react";
+import './Fallback.scss'; // Assure-toi de bien inclure ce fichier SCSS
+
 export default function Fallback() {
+  // Déclare une variable d'état pour stocker le pourcentage
+  const [progress, setProgress] = useState(1);
+
+  // Utilise useEffect pour mettre à jour le pourcentage de chargement
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Incrémente le pourcentage jusqu'à 100%
+      setProgress((prevProgress) =>
+        prevProgress < 100 ? prevProgress + 1 : 100
+      );
+    }, 100); // Change toutes les 100ms (0.1s)
+
+    // Nettoie l'intervalle lorsque le composant est démonté
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="fallback">
       <div className="loader">
-        <h1>The scene is loading...</h1>
-        <span>1 %</span>
+        <h1>Chargement</h1>
+        <span>{progress} %</span> {/* Affiche la progression */}
+        
+        {/* Barre de progression */}
+        <div className="progress-bar">
+          <div className="progress" style={{ width: `${progress}%` }}></div>
+        </div>
       </div>
       <div className="info">
-        <p>Texte</p>
+        <p>découvrez le monde</p>
       </div>
     </div>
   );
