4e650318ebf2a842f64fd28a635d4013bf156400
ACORBERAND <antoinecobertondefleury@gmail.com>
2024-09-24 12:30:52 +0200

    modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import Fallback from "./components/Fallback";
 import Scene from "./components/Scene";
+import Modal from "./components/Modal";
 import "./styles/index.scss";
 
 function App() {
   return (
-    <Suspense fallback={<Fallback />}>
-    <Scene />
-    </Suspense>
+  //  <Suspense fallback={<Fallback />}>
+  //  <Scene />
+  //  </Suspense>
+  <Modal></Modal>
   );
 }
 
